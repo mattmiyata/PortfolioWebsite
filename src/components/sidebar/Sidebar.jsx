71a889863db21841ea0a37a29aef4b7e89584d2f
Matthew Miyata <mattmiyata@gmail.com>
2024-02-16 18:03:02 +0900

    commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -24,13 +24,21 @@ const variants = {
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
-      <motion.div className="bg" variants={variants}>
-        <Links setOpen={setOpen} />
-      </motion.div>
+    <>
+      {open ? (
+        <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+          <motion.div className="bg" variants={variants}>
+            <Links setOpen={setOpen} />
+          </motion.div>
 
-      <ToggleButton setOpen={setOpen} />
-    </motion.div>
+          <ToggleButton setOpen={setOpen} />
+        </motion.div>
+      ) : (
+        <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+          <ToggleButton setOpen={setOpen} />
+        </motion.div>
+      )}
+    </>
   );
 };
 
